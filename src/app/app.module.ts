4da6c713e516cb0af5b4b27279a8dda1a24e7e23
TheProdigy <luisale0410@gmail.com>
2022-01-27 12:16:37 -0400

    app module commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,18 +6,12 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { LoginRegisterComponent } from './login-register/login-register.component';
 import { AngularMaterialModule } from './angular-material/angular-material.module';
-import { HeaderComponent } from './header/header.component';
-import { CarouselComponent } from './carousel/carousel.component';
 import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginRegisterComponent,
-    HeaderComponent,
-    CarouselComponent,
     LoginComponent
   ],
   imports: [
